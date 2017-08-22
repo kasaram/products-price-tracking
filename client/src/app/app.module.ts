2e45eb33212ca45dcc516003fa6078243836021f
Muhammad Khan <muhammadkh4n@gmail.com>
2017-08-23 00:16:57 +0500

    Added signup form
    - added signup component
    - added valid email directive (not woring)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
 
 // Routes
 import { AppRoutes } from './app.routes';
+
+// Directives
+import { EmailValidatorDirective } from './directives/email.directive';
+
+// Components
+import { AppComponent } from './app.component'; // Root Component
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { WorkComponent } from './components/work/work.component';
 import { PriceComponent } from './components/price/price.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,9 @@ import { PriceComponent } from './components/price/price.component';
     AboutComponent,
     HomeComponent,
     WorkComponent,
-    PriceComponent
+    PriceComponent,
+    SignupComponent,
+    EmailValidatorDirective
   ],
   imports: [
     BrowserModule,
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { WorkComponent } from './components/work/work.component';
 import { PriceComponent } from './components/price/price.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'work', component: WorkComponent },
-  { path: 'price', component: PriceComponent }
+  { path: 'price', component: PriceComponent },
+  { path: 'auth/signup', component: SignupComponent }
 ];
 
 export const AppRoutes = RouterModule.forRoot(routes);
