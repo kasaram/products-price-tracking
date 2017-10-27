5b0d65500f8e4ee48a5393d2957b541c2e4f0ab5
Muhammad Khan <muhammadkh4n@gmail.com>
2017-10-27 08:39:05 +0500

    universal route

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 
 const router = express.Router();
 
-router.post('/deploy', (req, res, next) => {
+router.any('/deploy', (req, res, next) => {
   exec('git pull origin deploy', (err, stdout, stderr) => {
     if (err) {
       console.log(err);
