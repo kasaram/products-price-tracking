c7b3f0048fce2dd01d9d4ae1f9fb56a4bdbd0be7
Muhammad Khan <muhammadkh4n@gmail.com>
2017-10-27 05:35:38 +0500

    fixed images in home, added environment vars

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://pricetrack.in'
+  apiUrl: 'http://pricetrack.in',
+  clientId: '254361041755193',
+  redirectUri: 'http://pricetrack.in'
 };
