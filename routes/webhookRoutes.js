518559b4cb2b6dfa19e4fa8220944db60df6aa0d
Muhammad Khan <muhammadkh4n@gmail.com>
2017-10-27 08:01:08 +0500

    added execution on webhook route

--- routes/webhookRoutes.js
+++ routes/webhookRoutes.js
@@ -4,7 +4,15 @@ const { exec } = require('child_process');
 const router = express.Router();
 
 router.post('/deploy', (req, res, next) => {
-  console.log(req.body);
+  exec('git pull origin deploy', (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log("STDOUT", stdout);
+    console.log("STDERR", stderr);
+    res.sendStatus(200);
+  });
 });
 
 module.exports = router;
